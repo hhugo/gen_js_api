24fcee83021a5238beb6ae4f5f1c122c58bfa32a
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-08-17 18:26:38 +0800

    integrate with ppxlib

--- ppx-lib/gen_js_api_ppx.mli
+++ ppx-lib/gen_js_api_ppx.mli
@@ -2,6 +2,10 @@
 (* See the attached LICENSE file.                                         *)
 (* Copyright 2015 by LexiFi.                                              *)
 
+val mark_as_handled_manually : (Parsetree.attribute -> unit) ref
+
+val check_attribute : bool ref
+
 val mapper : Ast_mapper.mapper
 
 val standalone : unit -> unit
